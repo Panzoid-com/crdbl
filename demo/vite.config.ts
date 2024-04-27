6f4c216c48a3c884f3644f4f5fa81b9483b0c657
Ryan Conrad <ryanconrad00@outlook.com>
2024-04-26 23:44:25 -0500

    Fix base path for demo app in vite config

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -3,11 +3,12 @@ import react from '@vitejs/plugin-react';
 import mkcert from 'vite-plugin-mkcert';
 
 // https://vitejs.dev/config/
-export default defineConfig({
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/crdbl/' : '/',
   plugins: [react(), mkcert()],
   server: {
     fs: {
       allow: ["../bindings/web/dist", "."]
     }
   }
-});
+}));
